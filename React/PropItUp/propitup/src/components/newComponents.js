be28af2452e97757e757b7c3499dc42212ae6b00
Stuart-Yee <ki6fzb@gmail.com>
2022-04-24 22:04:48 -0700

    using state and props

--- React/PropItUp/propitup/src/components/newComponents.js
+++ React/PropItUp/propitup/src/components/newComponents.js
@@ -7,6 +7,7 @@ class PersonCard extends Component{
                 <h2>{this.props.lName}, {this.props.fName}</h2>
                 <p>Age: {this.props.age }</p>
                 <p>Hair Color: {this.props.hColor}</p>
+                <button onClick={()=>alert(`My name is ${this.props.fName} ${this.props.lName}`)}>Click!</button>
             </div>
         );
     }
