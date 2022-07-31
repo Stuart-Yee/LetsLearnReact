b4381e9307959d757dfcad8d6e85d5dcbf4df35b
Stuart-Yee <ki6fzb@gmail.com>
2022-07-31 12:49:51 -0700

    completed to-do list app

--- React/boxgen/src/App.js
+++ React/boxgen/src/App.js
@@ -4,8 +4,9 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [colors, setColors] = useState([])
+  const [colors, setColors] = useState([]) //make an array of colors
   
+  // add new colors to this array
   const addColor = (color) => {
     console.log("Calling addColor");
     setColors([...colors, color]);
